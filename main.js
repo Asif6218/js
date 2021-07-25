566b39b7b270d5d15a71199ddc4d2058cacecd09
Asif6218 <mohdasif6227@gmail.com>
2021-07-24 23:01:25 -0600

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+// function sum(a,b){
+//     var add = a+b;
+//     return add
+// }
+// var res = add(4,5);
+// console.log(res);
+
+function sample(){
+    console.log("hello");
+}
+sample();
+
+function add(a,b){
+   var c= a+b;
+   return c;
+}
+var res= add(2,3);
+console.log(res)
